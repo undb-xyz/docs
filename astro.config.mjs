27a03db0faaf9cfbcfa62af1bfeacd6b16d5ad68
nichenqin <nichenqin@hotmail.com>
2023-04-12 10:44:49 +0800

    fix: fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config'
-import preact from '@astrojs/preact'
-import react from '@astrojs/react'
-
 import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
