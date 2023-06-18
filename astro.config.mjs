e9f9aefb96a7421e8828ae988fc9da5b2ed5e2ac
Sophie <wanshufen0414@gmail.com>
2023-06-18 22:20:12 +0800

    Add subscribe API to docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,49 +1,53 @@
-import { defineConfig } from 'astro/config'
-import starlight from '@astrojs/starlight'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'undb Docs',
+      title: "undb Docs",
       logo: {
-        src: '/src/assets/logo.svg',
+        src: "/src/assets/logo.svg",
       },
       social: {
-        github: 'https://github.com/undb-xyz/undb',
-        discord: 'https://discord.gg/3rcNdU3y3U',
-        twitter: 'https://twitter.com/unified_undb',
+        github: "https://github.com/undb-xyz/undb",
+        discord: "https://discord.gg/3rcNdU3y3U",
+        twitter: "https://twitter.com/unified_undb",
       },
       sidebar: [
         {
-          label: 'Installation',
-          autogenerate: { directory: 'installation' },
+          label: "Installation",
+          autogenerate: { directory: "installation" },
         },
         {
-          label: 'Database',
-          autogenerate: { directory: 'database' },
+          label: "Database",
+          autogenerate: { directory: "database" },
         },
         {
-          label: 'Cache',
-          autogenerate: { directory: 'cache' },
+          label: "Cache",
+          autogenerate: { directory: "cache" },
         },
         {
-          label: 'Feature',
-          autogenerate: { directory: 'feature' },
+          label: "Feature",
+          autogenerate: { directory: "feature" },
         },
         {
-          label: 'Open API',
-          autogenerate: { directory: 'openapi' },
+          label: "Open API",
+          autogenerate: { directory: "openapi" },
         },
         {
-          label: 'Webhook',
-          autogenerate: { directory: 'webhook' },
+          label: "Webhook",
+          autogenerate: { directory: "webhook" },
         },
         {
-          label: 'Contribution',
-          autogenerate: { directory: 'contribution' },
+          label: "Subscribe",
+          autogenerate: { directory: "subscribe" },
+        },
+        {
+          label: "Contribution",
+          autogenerate: { directory: "contribution" },
         },
       ],
     }),
   ],
-})
+});
