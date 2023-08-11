f93f8a2812d15eaf7455a502aa33910bee6c70c3
nichenqin <nichenqin@hotmail.com>
2023-08-11 11:53:17 +0800

    rls: update rls documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,56 +8,56 @@ export default defineConfig({
   },
   integrations: [
     starlight({
-      title: "undb Docs",
+      title: 'undb Docs',
       logo: {
-        src: "/src/assets/logo.svg",
+        src: '/src/assets/logo.svg',
       },
       editLink: {
-        baseUrl: "https://github.com/undb-xyz/docs/edit/main",
+        baseUrl: 'https://github.com/undb-xyz/docs/edit/main',
       },
       social: {
-        github: "https://github.com/undb-xyz/undb",
-        discord: "https://discord.gg/3rcNdU3y3U",
-        twitter: "https://twitter.com/unified_undb",
+        github: 'https://github.com/undb-xyz/undb',
+        discord: 'https://discord.gg/3rcNdU3y3U',
+        twitter: 'https://twitter.com/unified_undb',
       },
       sidebar: [
         {
-          label: "Installation",
-          autogenerate: { directory: "installation" },
+          label: 'Installation',
+          autogenerate: { directory: 'installation' },
         },
         {
-          label: "Database",
-          autogenerate: { directory: "database" },
+          label: 'Database',
+          autogenerate: { directory: 'database' },
         },
         {
-          label: "Cache",
-          autogenerate: { directory: "cache" },
+          label: 'Cache',
+          autogenerate: { directory: 'cache' },
         },
         {
-          label: "Feature",
-          autogenerate: { directory: "feature" },
+          label: 'Feature',
+          autogenerate: { directory: 'feature' },
         },
         {
-          label: "Open API",
-          autogenerate: { directory: "openapi" },
+          label: 'Open API',
+          autogenerate: { directory: 'openapi' },
         },
         {
-          label: "Webhook",
-          autogenerate: { directory: "webhook" },
+          label: 'Webhook',
+          autogenerate: { directory: 'webhook' },
         },
         {
-          label: "Subscribe",
-          autogenerate: { directory: "subscribe" },
+          label: 'Subscribe',
+          autogenerate: { directory: 'subscribe' },
         },
         {
-          label: "Record Level Security",
-          autogenerate: { directory: "recordsecurity" },
+          label: 'Authorization',
+          autogenerate: { directory: 'authorization' },
         },
         {
-          label: "Contribution",
-          autogenerate: { directory: "contribution" },
+          label: 'Contribution',
+          autogenerate: { directory: 'contribution' },
         },
       ],
     }),
   ],
-});
+})
